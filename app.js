7aeb3db19588777f76849208e6ad554ab18aa948
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-10 12:22:40 +0530

    err 404 page is updated

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// const errorControllers = require('./controller/error')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
