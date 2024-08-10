82e2f24f8684f9d8acf76c8583df47d9c9a909f8
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-10 14:16:06 +0530

    Task 6 is completed here

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorControllers.get404Error);
+app.use(errorControllers.get404);
 
 app.listen(3000);
--- controller/error.js
+++ controller/error.js
@@ -1,4 +1,4 @@
-exports.get404Error = (req, res, next) => {
+exports.get404 = (req, res, next) => {
     res.status(404).render('404', {
         path: req.path,
         pageTitle: 'Page Not Found' 
